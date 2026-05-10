57be6cfe3db75eede0a5bf9080fccb5ae1dc2c6f
murillo <murillofrazaocunha@gmail.com>
2026-05-10 17:23:30 -0300

    refactor: improve tag fetching logic and enhance obfuscation configuration

--- app/controllers/Admin/CoreController.php
+++ app/controllers/Admin/CoreController.php
@@ -2,7 +2,7 @@
 
 namespace App\controllers\Admin;
 
-require_once __DIR__ . '/../../utils/CoreUtils.php';
+require_once __DIR__ . '/../../Utils/CoreUtils.php';
 use models\Core;
 use models\Server;
 use Vatts\Router\Request;
